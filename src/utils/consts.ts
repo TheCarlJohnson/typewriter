c43b8c8a176b573af993ad426e8d16e6b833f302
TheCarlJohnson <85150550+TheCarlJohnson@users.noreply.github.com>
2026-03-21 21:04:47 +0530

    Update consts.ts

--- src/utils/consts.ts
+++ src/utils/consts.ts
@@ -1,16 +1,10 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
 import { getEntry } from 'astro:content';
-
 export async function getSite() {
-	// Try to read the `site` entry and fallback to constants
-	const entry = await getEntry('site', 'site');
-	const author = await getEntry('site', 'author');
+	const entry = await getEntry('site', 'config');
 	return {
 		title: entry?.data.title,
 		description: entry?.data.description,
 		hero: entry?.data.hero,
-		author: author?.data,
+		author: entry?.data.author,
 	};
 }
